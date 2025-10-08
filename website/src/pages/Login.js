92c361b2e1369f9a966941054f0da76c0711249c
Justin Akinyi <justakinyi1@gmail.com>
2025-10-08 13:55:31 +0000

    updates

--- website/src/pages/Login.js
+++ website/src/pages/Login.js
@@ -1,8 +1,17 @@
-// src/pages/Login.js
+// // src/pages/Login.js
+// import React from "react";
+// import LearningPage from "./Learning";
+
+// export default function Login() {
+//   // ⚠️ TEMPORARY: redirect straight to LearningPage
+//   return <LearningPage />;
+// }
 import React from "react";
-import LearningPage from "./Learning";
 
 export default function Login() {
-  // ⚠️ TEMPORARY: redirect straight to LearningPage
-  return <LearningPage />;
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>This page is under development</h1>
+    </div>
+  );
 }
