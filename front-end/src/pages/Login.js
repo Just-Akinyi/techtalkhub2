fae456e6559d1eafff0a1dc3bde74e617dfe07d4
Justin Akinyi <justakinyi1@gmail.com>
2025-10-08 00:43:48 +0000

    upgrades

--- front-end/src/pages/Login.js
+++
@@ -1,8 +0,0 @@
-// src/pages/Login.js
-import React from "react";
-import LearningPage from "./Learning";
-
-export default function Login() {
-  // ⚠️ TEMPORARY: redirect straight to LearningPage
-  return <LearningPage />;
-}
---
+++ shared/firebase.js
@@ -0,0 +1,19 @@
+// src-shared/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "yourproject.firebaseapp.com",
+  projectId: "yourproject",
+  storageBucket: "yourproject.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
