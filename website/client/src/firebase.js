79ad2e950675973ad60326a5671cad02922b1464
Justin Akinyi <justakinyi1@gmail.com>
2025-10-10 04:30:04 +0000

    costly commit

--- website/client/src/firebase.js
+++ website/client/src/firebase.js
@@ -2,9 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBvkomr1-qVjsqM0vNPG2X13jvZVJR5QRY",
-  authDomain: "tech-talk-hub.firebaseapp.com",
-  projectId: "tech-talk-hub",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   // storageBucket: "tech-talk-hub.firebasestorage.app",
   storageBucket: "tech-talk-hub.appspot.com",
   messagingSenderId: "457529885849",
